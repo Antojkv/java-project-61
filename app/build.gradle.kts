045d86e8feff6559e20615109bfa33ea8f10a239
Антон Кузянин <marswaitsfor@gmail.com>
2025-10-07 22:46:54 +0300

    Add java-project-61 with Gradle configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("java")
+    id("application")
+    id("se.patrikerdes.use-latest-versions") version "0.2.19"
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.apache.commons:commons-lang3:3.19.0")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("--enable-preview")
+}
+
+tasks.withType<JavaExec> {
+    jvmArgs("--enable-preview")
+}
