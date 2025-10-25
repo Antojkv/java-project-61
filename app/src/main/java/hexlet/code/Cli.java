368627019863dd6aa57d16bff8051f90cef6f973
Antojkv <marswaitsfor@gmail.com>
2025-10-25 12:48:54 +0300

    Update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,7 +7,7 @@ public final class Cli {
     }
     public static String greetUser() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("May i have your name? ");
+        System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
         return userName;
